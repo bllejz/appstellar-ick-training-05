567abd64dd129f34d7af329f3637f52fe2d7c15b
bllejz <arberhash@gmail.com>
2023-06-23 19:40:20 +0200

    aws netw interface added now

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_network_interface" "web" {
   subnet_id       = "subnet-03ddf0d4fd589ae9a"
-  security_groups = [“sg-0dd3a367880737f7a”]
+  security_groups = ["sg-0dd3a367880737f7a"]
 }
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
