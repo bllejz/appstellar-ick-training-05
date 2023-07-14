3c773920c3ad56340f8a842490a7f4e796d72493
bllejz <arberhash@gmail.com>
2023-07-14 20:52:05 +0200

    added s3 bucket

--- terraform-lab/main.tf
+++ terraform-lab/main.tf
@@ -7,3 +7,12 @@ data "aws_vpc" "this"{
 output "vpc_id" {
     value = data.aws_vpc.this.id
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-bllejz"
+
+  tags = {
+    Name        = "Ick Training"
+    Environment = "Dev"
+  }
+}
